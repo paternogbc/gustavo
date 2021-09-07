0ccbd2579049e402fa5c3f3a402419c68f47e224
Gustavo Paterno <paternogbc@gmail.com>
2021-09-07 12:13:16 +0200

    initial commit.

---
+++ R/build_project.R
@@ -0,0 +1,25 @@
+# Create basic reproducible analysis project
+build_project <- function(type = "analysis",
+                          temp = TRUE) {
+
+  if(type == "analysis"){
+    # Data
+    dir.create(path = here::here("data"))
+    dir.create(path = here::here("data", "data_raw"))
+    dir.create(path = here::here("data", "data_derived"))
+
+    # outputs
+    dir.create(path = here::here("outputs"))
+    dir.create(path = here::here("outputs", "figures"))
+    dir.create(path = here::here("outputs", "tables"))
+    if(isTRUE(temp)){
+      dir.create(path = here::here("outputs", "temp"))
+    }
+
+    # scripts
+    dir.create(path = here::here("scripts"))
+
+    # docs
+    dir.create(path = here::here("scripts"))
+  }
+}
